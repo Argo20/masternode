8816379479cf8fac4152a8b81ea60919a79d1391
c1xx <c1xx@pm.me>
2018-08-27 10:22:00 +0200

    Crontab entry fix

--- masternode-setup.sh
+++ masternode-setup.sh
@@ -235,7 +235,7 @@ git clone https://github.com/Argo20/sentinel.git && cd sentinel >/dev/null 2>&1
 virtualenv ./venv >/dev/null 2>&1
 ./venv/bin/pip install -r requirements.txt >/dev/null 2>&1
 ./venv/bin/python bin/sentinel.py >/dev/null 2>&1
-echo "* * * * * cd $DATAFOLDER/sentinel && ./venv/bin/python bin/sentinel.py >/dev/null 2>&1" >> /var/spool/cron/crontabs/root >/dev/null 2>&1
+echo "* * * * * cd $DATAFOLDER/sentinel && ./venv/bin/python bin/sentinel.py >/dev/null 2>&1" >> /var/spool/cron/crontabs/root
 
 echo 'argo_conf='$CONFIGFOLDER/$CONFIG_FILE$'
 network=mainnet
